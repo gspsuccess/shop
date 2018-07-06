e256ff0cb6e7f91c827e8403a4519ccfa4cd6738
vagrant <vagrant@homestead>
2018-07-06 10:10:00 +0000

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-});
-
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-    // 开始
-    Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
-    });
-    // 结束
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
 });

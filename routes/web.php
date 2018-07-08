004eb1de529f8dab6a0651f9d6af5f9eeb701768
gspsuccess <gspsuccess@163.com>
2018-07-08 01:39:55 +0000

    user page product details

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,4 @@ Route::group(['middleware' => 'email_verified'], function() {
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');

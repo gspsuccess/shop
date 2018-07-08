b40f3680fd74a21547760f47353e37e059279ac2
gspsuccess <gspsuccess@163.com>
2018-07-08 01:48:32 +0000

    user page product fav

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,18 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    Route::group(['middleware' => 'email_verified'], function() {
+        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+        Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+        Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+        Route::get('user_addresses/{address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+        Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+        Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+        Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+        Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    });
 });
 
-Route::group(['middleware' => 'email_verified'], function() {
-    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
-    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
-    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
-    Route::get('user_addresses/{address}', 'UserAddressesController@edit')->name('user_addresses.edit');
-    Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
-    Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
-});
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
